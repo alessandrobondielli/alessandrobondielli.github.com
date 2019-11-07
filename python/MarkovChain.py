274698969a2d8970d96750579c96487cd04458c1
Alessandro Bondielli <alessandrobondielli@Alessandros-MacBook-Pro.local>
2019-11-07 10:54:54 +0100

    uptaded markovchain file with numpy import

--- python/MarkovChain.py
+++ python/MarkovChain.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
+import numpy.random as random
 
 class MarkovChain:
     """Simple Markov Chain Model.
@@ -34,12 +35,12 @@ class MarkovChain:
 
     def __init__(self, n, P, T, states=None, verbose= False):
         """Simple Markov chain model.
-        
+
         """
         assert len(P) == n, "Probability vector should be of size %d" %n
         assert len(T) == len(T[0]) and len(T) == n, "Transition matrix should be of size %d" %n
         assert states is None or len(states) == n, "States vector should be of size $d" %n
-        
+
         # Number of states of the MarkovChain
         self.n = n
         self.states = states
@@ -47,7 +48,7 @@ class MarkovChain:
         self.T = T
         self.state = random.choice(range(self.n),1,p=self.p)[0]
         self.verbose = verbose
-        
+
     def check_state(self):
         state = self.states[self.state] if self.states else self.state
         if self.verbose:
@@ -56,10 +57,10 @@ class MarkovChain:
     def set_state(self, state):
         """Set the state for the MarkovChain to the specified state.
         Can be used for initialization.
-        
+
         Parameters
         ----------
-        state : int, or string 
+        state : int, or string
             the state to set; can be either the index of the state,
             or its string label, if labels have been provided at initialization
             time.
@@ -71,34 +72,34 @@ class MarkovChain:
         if self.verbose:
             state = self.states[self.state] if self.states else self.state
             print 'State is now: %s' % (state)
-        
+
     def get_state(self):
         """Get the state of the markov chain.
-        
+
         Parameters
         ----------
-        
+
         Returns
         -------
-        state : int, or string 
+        state : int, or string
             the current state to of the Markov Chain; can be either the index of the state,
             or its string label, if labels have been provided at initialization
             time.
         """
         state = self.states[self.state] if self.states else self.state
         return state
-        
+
     def move(self):
         """Sample num_samples samples from the Markov Model.
-        
+
         Parameters
         ----------
-        
+
         Returns
         -------
         mutated : boolean
            true if the the model state has changed during the current update.
-           
+
         """
         current_state = self.state
         self.state = random.choice(range(self.n),1,p=self.T[current_state])[0]
@@ -107,23 +108,23 @@ class MarkovChain:
             print 'New State: %s' % (state)
         return self.state != current_state
 
-        
+
 # Jukes e Cantor
 # alpha < 1/3.
 def JC69(nucleotide):
     """Initialize a Markov Chain for a nucleotide, using a Jukes e Cantor model.
-    
+
     Parameters
     ----------
     nucleotide: string, must be one of ['A', 'C', 'G', 'T']
         A nucleotide used to initialize the model.
-    
+
     Returns
     -------
     mc : MarkovChain object.
        The MarkovChain object, using a Jukes Cantor Model initialized to the provided
        nucleotide.
-       
+
     Examples
     --------
     >>> nuc_mc = JC69('A')
@@ -136,7 +137,7 @@ def JC69(nucleotide):
     >>> nuc_mc.get_state()
     'C'
     """
-    n = 4 
+    n = 4
     prob = [0.25, 0.25, 0.25, 0.25]
     alpha = 0.005
     JC69_sub = [[1-alpha*3, alpha, alpha, alpha],
@@ -149,10 +150,10 @@ def JC69(nucleotide):
     return mc
 
 #plot della distribuzione
-  
+
 def plot_gen(total_arr):
     total_v = total_arr.mean(axis=0)
-    
+
     fig = plt.figure()
     ax = plt.plot(total_v,range(length*5))
     plt.plot(range(length),range(length))
